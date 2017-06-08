83c909eb680773462e1c0fefea8fdfe20cbed6bd
FerdinandPiette <ferdinand.piette@yncrea.fr>
2017-06-08 08:53:41 +0000

    adding fakeapi.js : a fake api emulating the springboot code

---
+++ fakeapi.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+// middleware that is specific to this router
+router.use(function timeLog (request, response, next) {
+  console.log('Time: ', Date.now());
+  next();
+})
+
+router.get('/add', function (request, response) {
+	response.send({
+		log: 'add not implemented yet'
+	});
+});
+
+router.get('/redeem', function (request, response) {
+	response.send({
+		log: 'redeem not implemented yet'
+	});
+});
+
+router.get('/view', function(request, response) {
+	response.send({
+		log: 'view not implemented yet'
+	});
+});
+
+module.exports = router
